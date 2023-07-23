91ff39fd4d3c764a7b899fbc1c7621d37838f741
elizamirzoyan <115018777+elizamirzoyan@users.noreply.github.com>
2023-07-23 13:13:18 +0400

    Update client.cpp

--- client.cpp
+++ client.cpp
@@ -17,19 +17,19 @@ int main() {
         return 1;
     }
 
-    // Preparing  the sockaddr_in structure
+    // preparing  the sockaddr_in structure
     
     sockaddr_in serverAddr;
     serverAddr.sin_family = AF_INET;
     serverAddr.sin_port = htons(port);
 
-    // Convert IPv4 and IPv6 addresses from text to binary form
+    // converting IPv4 and IPv6 addresses from text to binary form
     if (inet_pton(AF_INET, serverIP.c_str(), &serverAddr.sin_addr) <= 0) {
         std::cerr << "Invalid address/Address not supported.\n";
         return 1;
     }
 
-    // Connect to server
+    // connectin to server
     if (connect(clientSocket, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) < 0) {
         std::cerr << "Connection failed.\n";
         return 1;
@@ -37,17 +37,17 @@ int main() {
 
     std::cout << "Connected to server.\n";
 
-    // Send message to server
+    // sending a message to server
     std::string message = "Hello from client!";
     send(clientSocket, message.c_str(), message.length(), 0);
     std::cout << "Message sent to server.\n";
 
-    // Receive message from server
+    // receiving a message from server
     char buffer[1024] = {0};
     int valread = read(clientSocket, buffer, 1024);
     std::cout << "Received: " << buffer << std::endl;
 
-    // Close connection
+    
     close(clientSocket);
 
     return 0;

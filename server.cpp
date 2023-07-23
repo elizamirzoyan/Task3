5ec1d5dfe9c9ea73d3a53f7eaca11843c5e80372
elizamirzoyan <115018777+elizamirzoyan@users.noreply.github.com>
2023-07-23 13:14:55 +0400

    Update server.cpp

--- server.cpp
+++ server.cpp
@@ -8,30 +8,30 @@
 int main() {
     const int port = 12345; 
 
-    // Create socket
+    // creating a socket
     int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
     if (serverSocket == -1) {
         std::cerr << "Error creating socket.\n";
         return 1;
     }
 
-    // Prepare the sockaddr_in structure
+    // preparing the sockaddr_in structure
     sockaddr_in serverAddr;
     serverAddr.sin_family = AF_INET;
     serverAddr.sin_addr.s_addr = INADDR_ANY;
     serverAddr.sin_port = htons(port);
 
-    // Bind
+    // binding
     if (bind(serverSocket, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) < 0) {
         std::cerr << "Error binding to port.\n";
         return 1;
     }
 
-    // Listen
+    // listennn
     listen(serverSocket, 3);
     std::cout << "Server started. Listening for connections...\n";
 
-    // Accept incoming connection
+    // accepting incoming connection
     sockaddr_in clientAddr;
     int addrLength = sizeof(clientAddr);
     int clientSocket = accept(serverSocket, (struct sockaddr *)&clientAddr, (socklen_t*)&addrLength);
@@ -44,17 +44,17 @@ int main() {
     inet_ntop(AF_INET, &(clientAddr.sin_addr), clientIP, INET_ADDRSTRLEN);
     std::cout << "Connection accepted from " << clientIP << std::endl;
 
-    // Receive message from client
+    // receiving a message from client
     char buffer[1024] = {0};
     int valread = read(clientSocket, buffer, 1024);
     std::cout << "Received: " << buffer << std::endl;
 
-    // Send message to client
+    // sending a message to client
     std::string message = "Hello from server!";
     send(clientSocket, message.c_str(), message.length(), 0);
     std::cout << "Message sent to client.\n";
 
-    // Close connection
+    // closing the connection
     close(clientSocket);
     close(serverSocket);
 
